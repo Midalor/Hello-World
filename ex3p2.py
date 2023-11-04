a04b9569fcdab05961c7d72a441d20f11812f728
VladimirFrolov-OIB <87655722+Midalor@users.noreply.github.com>
2023-11-04 22:07:32 +0300

    ex3p2.py

--- ex3p2.py
+++ ex3p2.py
@@ -1,5 +1,23 @@
-vowels = 'aeiuy' 
-word = (input("Введите слово: ")).lower() 
-print('количество согласных', 
-len(list(filter(lambda x: not x in vowels,word))))
-print('количество гласных', len(list(filter(lambda x: x in vowels, word))))
+glasn= ['a','e','i','u','y','o']
+sogl= ['q','w','r','t','p','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m']
+word= input("Введите слово: ")
+x=0
+z=0
+y=0
+for g in glasn:
+    for letter in word:
+        if letter == g:
+            x+=1
+    if x == 0:
+            print(g, ":False")
+    else:
+        print(g, ":", x)
+    y+=x
+    x=0
+for s in sogl:
+    for letter in word:
+        if letter == s:
+            z+=1
+print("Согласных: ", z)
+y+=z
+print("Всего Букв:", y)
