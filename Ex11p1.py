fe748b8bf761c03a6faa26251588385ac9f38f36
VladimirFrolov-OIB <87655722+Midalor@users.noreply.github.com>
2023-12-05 22:22:23 +0300

    Update Ex11p1.py
    Задание было выполнено не правильно, исправил

--- Ex11p1.py
+++ Ex11p1.py
@@ -1,17 +1,7 @@
-a= int(input("Введите число: "))
-n=a
-m=a
-spisok=[]
-while n > 1:
-    n=n-1
-    m*=n
-for lenght in range(m, 1, -1):
-    i=lenght
-    y=lenght
-    while y > 1:
-        y=y-1
-        i*=y
-    spisok.append(i)
-spisok.append(y)
+def f(i): return 1 if i == 1 else i * f(i - 1) 
+n = int(input("Введите значение: ")) 
+f1 = f(n)
+print(f1)
+spisok = [f(f1 - i) for i in range(f1)] 
 print(spisok)
-    
+
